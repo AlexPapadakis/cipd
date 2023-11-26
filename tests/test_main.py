32845fd36f3d63e6b5605c0f79b12d8cc733b533
AlexPapadakis <alexpap22102001@gmail.com>
2023-11-26 14:53:29 +0200

    added multiplication

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,5 @@
 import unittest
-from main import add_numbers, subtract_numbers
+from main import add_numbers, subtract_numbers, multiply_numbers
 
 class TestMainFunctions(unittest.TestCase):
     def test_add_numbers(self):
@@ -8,4 +8,6 @@ class TestMainFunctions(unittest.TestCase):
     def test_subtract_numbers(self):
         self.assertEqual(subtract_numbers(5, 3), 2)
 
+    def test_multiply_numbers(self):
+        self.assert_Equal(multiply_numbers(5,3),15)
         
